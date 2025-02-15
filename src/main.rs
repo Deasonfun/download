12632b9ea29a6524d3fdfc79bde0f39c2e5b2bd9
Deasonfun <dftothemax@gmail.com>
2025-02-15 14:13:26 -0500

    Uses CSV file to input URLs for processing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fs::File;
 use std::path::PathBuf;
 use std::process::Command;
+
 use yt_dlp::Youtube;
 
 #[tokio::main]
@@ -7,14 +9,23 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let execs_dir = PathBuf::from("libs");
     let output_dir = PathBuf::from("output");
 
-    let fetcher = Youtube::with_new_binaries(&execs_dir, output_dir).await?;
+    let _fetcher = Youtube::with_new_binaries(&execs_dir, output_dir).await?;
 
     let dlp_bin = execs_dir.join("yt-dlp");
 
-    Command::new(dlp_bin)
-        .arg("https://www.youtube.com/watch?v=WbvNur3L-yc")
-        .spawn()
-        .expect("youtube-dlp command failed");
+    println!("yes");
+    let videos_list_csv = File::open("videos.csv")?;
+    let mut rdr = csv::Reader::from_reader(videos_list_csv);
+    for result in rdr.records() {
+        let record = result?;
+        println!("{:?}", &record[0]);
+        let output = Command::new(&dlp_bin)
+            .arg(&record[0])
+            .output()
+            .expect("youtube-dlp command failed");
+        println!("{}", output.status);
+    }
+
     //let url = String::from("https://www.youtube.com/watch?v=WbvNur3L-yc");
     //let video_path = fetcher.download_video_from_url(url, "video.mp4").await?;
     Ok(())
