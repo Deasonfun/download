87bb2eaae0f20ce727262fd2cab39e13b5893224
Deasonfun <dftothemax@gmail.com>
2025-02-15 23:58:17 -0500

    Removed youtube-dlp package and libs download

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,12 @@ use std::fs::File;
 use std::path::PathBuf;
 use std::process::Command;
 
-use yt_dlp::Youtube;
-
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let execs_dir = PathBuf::from("libs");
-    let output_dir = PathBuf::from("output");
-
-    let _fetcher = Youtube::with_new_binaries(&execs_dir, output_dir).await?;
 
     let dlp_bin = execs_dir.join("yt-dlp");
 
-    println!("yes");
     let videos_list_csv = File::open("videos.csv")?;
     let mut rdr = csv::Reader::from_reader(videos_list_csv);
     for result in rdr.records() {
@@ -26,7 +20,5 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("{}", output.status);
     }
 
-    //let url = String::from("https://www.youtube.com/watch?v=WbvNur3L-yc");
-    //let video_path = fetcher.download_video_from_url(url, "video.mp4").await?;
     Ok(())
 }
