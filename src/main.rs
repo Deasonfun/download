eb41ee7e144289ff2f2d9aa458dcee29aab8488b
Kyle Deason <kyledeason@Kyles-MacBook-Pro-2.local>
2025-10-26 15:22:10 -0400

    Replaced CSV config with JSON

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::path::PathBuf;
 use std::process::Command;
 
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    videos: Vec<String>,
+}
+
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let execs_dir = PathBuf::from("libs");
 
     let dlp_bin = execs_dir.join("yt-dlp");
 
-    let videos_list_csv = File::open("videos.csv")?;
-    let mut rdr = csv::Reader::from_reader(videos_list_csv);
-    for result in rdr.records() {
-        let record = result?;
+    let config_json = fs::read_to_string("config.json")?;
+    let config: Config = serde_json::from_str(&config_json)?;
+
+    //let videos_list_csv = File::open("videos.csv")?;
+    //let mut rdr = csv::Reader::from_reader(videos_list_csv);
+    for result in config.videos {
+        let record = result;
+
+        println!("{}", record);
 
         let output = Command::new(&dlp_bin)
-            .args(["-f mp4", &record[0]])
+            .args(["-f mp4", &record])
             .output()?;
-        println!("{:?}", output);
         println!("{}", output.status);
     }
 
