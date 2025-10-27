1b605b0d08847e1e7819f79161b61fd212045477
k <k@plaptop>
2025-10-27 11:58:47 -0400

    Set up an option to export the audio

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
 struct Config {
+    audio_export: bool,
+    audio_format: String,
     videos: Vec<String>,
 }
 
@@ -15,9 +17,26 @@ pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let execs_dir = PathBuf::from("libs");
 
     let dlp_bin = execs_dir.join("yt-dlp");
+    let ffmpeg_bin = execs_dir.join("ffmpeg");
 
     let config_json = fs::read_to_string("config.json")?;
     let config: Config = serde_json::from_str(&config_json)?;
+
+    let mut command_args = vec!["-f mp4"];
+
+    let audio_format = config.audio_format;
+    println!("Audio format: {}", audio_format);
+
+    match config.audio_export {
+        true => {
+            command_args.push("-x");
+            command_args.push("--audio-format");
+            command_args.push(audio_format.as_str());
+            command_args.push("--ffmpeg-location");
+            command_args.push(ffmpeg_bin.to_str().unwrap());
+        },
+        false => ()
+    }
 
     //let videos_list_csv = File::open("videos.csv")?;
     //let mut rdr = csv::Reader::from_reader(videos_list_csv);
@@ -25,11 +44,15 @@ pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         let record = result;
 
         println!("{}", record);
+        println!("{:?}", &command_args);
 
         let output = Command::new(&dlp_bin)
-            .args(["-f mp4", &record])
+            .args(&command_args)
+            .arg(record.clone())
             .output()?;
         println!("{}", output.status);
+
+        
     }
 
     Ok(())
