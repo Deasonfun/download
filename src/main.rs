0f451635661141c01ffa3249a292c66f51e21ea0
k <k@plaptop>
2025-10-27 16:43:26 -0400

    Added video format option

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::fs::{self, File};
+use std::fs::{self};
 use std::path::PathBuf;
 use std::process::Command;
 
 use serde::{Deserialize, Serialize};
-use serde_json::Result;
 
 #[derive(Serialize, Deserialize)]
 struct Config {
+    video_format: String,
     audio_export: bool,
     audio_format: String,
     videos: Vec<String>,
@@ -14,6 +14,7 @@ struct Config {
 
 #[tokio::main]
 pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
+
     let execs_dir = PathBuf::from("libs");
 
     let dlp_bin = execs_dir.join("yt-dlp");
@@ -22,13 +23,18 @@ pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let config_json = fs::read_to_string("config.json")?;
     let config: Config = serde_json::from_str(&config_json)?;
 
-    let mut command_args = vec!["-f mp4"];
+
+    let mut command_args = vec![];
+
+    command_args.push("-f");
+    command_args.push(config.video_format.as_str());
+    println!("Video format: {}", config.video_format);
 
     let audio_format = config.audio_format;
-    println!("Audio format: {}", audio_format);
 
     match config.audio_export {
         true => {
+            println!("Audio format: {}", audio_format);
             command_args.push("-x");
             command_args.push("--audio-format");
             command_args.push(audio_format.as_str());
@@ -38,21 +44,18 @@ pub async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
         false => ()
     }
 
-    //let videos_list_csv = File::open("videos.csv")?;
-    //let mut rdr = csv::Reader::from_reader(videos_list_csv);
+
     for result in config.videos {
         let record = result;
 
         println!("{}", record);
-        println!("{:?}", &command_args);
 
         let output = Command::new(&dlp_bin)
             .args(&command_args)
             .arg(record.clone())
             .output()?;
         println!("{}", output.status);
-
-        
+        println!("{:?}", output);
     }
 
     Ok(())
